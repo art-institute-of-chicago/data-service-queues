1621dff7cb7f7042707278da9269a23d3fc37beb
nikhiltri <ntrivedi@artic.edu>
2020-07-22 14:27:10 -0500

    Change file_get_contents to curl requests [WEB-1752]

--- app/SourceCallable.php
+++ app/SourceCallable.php
@@ -9,7 +9,19 @@ trait SourceCallable
     {
 
         $url = config('source.api_url') . $path;
-        return file_get_contents($url);
+        $proxy = env('CURL_PROXY');
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_PROXY, $proxy);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $contents = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $contents;
 
     }
 
